32c15319a58d321eeb17433681115c8600ff6093
Valerii-Postrybailo <postribailo.valeri@gmail.com>
2022-09-11 01:15:34 +0300

    .

--- src/fetch_picture.js
+++ src/fetch_picture.js
@@ -2,7 +2,7 @@ const BACK_END_URL =  'https://pixabay.com/api/'
 const API_KEY = '29743912-8e7685db13f3781653d214456'
 
 function fetchImg(qName) {
-  return fetch(`${BACK_END_URL}/?key=${API_KEY}&q=${qName}&image_type=photo&orientation=horizontal&safesearch=true`)
+  return fetch(`${BACK_END_URL}/?key=${API_KEY}&q=${qName}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`)
       .then(response => {
         if (response.status === 404) {
           return Promise.reject(new Error());
