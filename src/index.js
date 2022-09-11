3e17c544085dbb50f85a8b0c872dbee0a6496571
Valerii-Postrybailo <postribailo.valeri@gmail.com>
2022-09-11 17:25:32 +0300

    add btn murkup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
+import { fetchImg } from './fetch_picture';
+
 const axios = require('axios').default;
 
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+// Описаний в документації
 import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
-
-import { fetchImg } from './fetch_picture';
 
 ///////////////////////////////////////////////////////////////////
 
@@ -17,6 +19,10 @@ console.log(inputEl)
 
 const galleryEl = document.querySelector(".gallery")
 console.log(galleryEl)
+
+const buttonEl = document.querySelector(`.load-more`)
+buttonEl.style.display = "none"
+console.log(buttonEl)
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -62,80 +68,53 @@ const renderMarkup = data => {
 
 inputEl.addEventListener('input', inputEvtHandler);
 
-// console.log(fetchImg("cat"))
-
-// fetchImg("cat")
-
-
-// inputEl.addEventListener("input", evt => {
-//   console.log(evt.target.value)
-//   evt.preventDefault();
-//   fetchImg(evt.target.value)
-//     .then((images) => renderImgGallery(images))
-//     .catch((error) => Notify.failure("Sorry, there are no images matching your search query. Please try again."));
-// })
-
-// formEl.addEventListener('submit', submitForm);
-  // ///////////////////////////////////////////////////////////////////
-// inputEl.addEventListener("input", submitForm)
-// function submitForm(evt) {
-//   evt.preventDefault();
-//   console.log(evt.target.value)
-
-//   fetchImg(evt.target.value)
-//     .then((images) => renderImgGallery(images))
-//     .catch((error) => Notify.failure("Sorry, there are no images matching your search query. Please try again."));
-// }
-
-//////////////////////////////////////////////////////////////////////
-// function handleSubmit(evt) {
-//   evt.preventDefault();
-//   console.log(evt.target.value)
-//   fetchImg()
-//     .then((images) => renderImgGallery(images))
-//     .catch((error) => Notify.failure("Sorry, there are no images matching your search query. Please try again."));
-// }
-
-// galleryEl.insertAdjacentHTML("afterbegin",imgMarkup)
-// const markup = renderImgGallery(images)
-//   galleryEl.insertAdjacentHTML("afterbegin",markup)
-
-
 function renderImgGallery(images) {
-  console.log("Im here?")
-  const markup = images.hits
-  console.log(markup)
+  console.log(images.hits)
+    console.log("Im here?")
+    const markup = images.hits
+    console.log(markup)
+  if (markup == []){
+    Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+    console.log("wtf")
+  }else{
+    return markup
+      .map(
+        ({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => 
+        `
+        <a class="gallery__item" href="${largeImageURL}">
 
-  return markup
-    .map(
-      ({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => 
-      `
-      <a class="link" href="${largeImageURL}">
-        <div class="photo-card">
-          <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-          <div class="info">
-            <p class="info-item">
-              <b>Likes</b>
-                ${likes}
-            </p>
-            <p class="info-item">
-              <b>Views</b>
-                ${views}
-            </p>
-            <p class="info-item">
-              <b>Comments</b>
-                ${comments}
-            </p>
-            <p class="info-item">
-              <b>Downloads</b>
-                ${downloads}
-            </p>
+            <div class="photo-card">
+
+              <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+
+              <div class="info">
+
+                <p class="info-item">
+                  <b>Likes</b>
+                    ${likes}
+                </p>
+
+                <p class="info-item">
+                  <b>Views</b>
+                    ${views}
+                </p>
+
+                <p class="info-item">
+                  <b>Comments</b>
+                    ${comments}
+                </p>
+
+                <p class="info-item">
+                  <b>Downloads</b>
+                    ${downloads}
+                </p>
+              </div>
           </div>
-        </div>
-      </a>
-      `
-    )
-  .join("");
+        </a>
+        `
+      )
+    .join("");
+  }
 }
 
-new SimpleLightbox('.gallery a', { captionDelay	:250 });
+new SimpleLightbox('.gallery a', { captionDelay: 250 });
